2fc3bfb79a343e92307f1d21562fb20df1671026
cyeongb <cyeongb@gmail.com>
2020-04-22 19:56:53 +0900

    0422 19:56

--- src/App.js
+++ src/App.js
@@ -1,40 +1,57 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-
-const movies=[
+const movies = [
   {
-    title:'FrozenⅡ',
-    poster :'https://www.slashfilm.com/wp/wp-content/images/frozen2-characters-hugging.jpg'
+    title: "FrozenⅡ",
+    poster:
+      "https://www.slashfilm.com/wp/wp-content/images/frozen2-characters-hugging.jpg",
   },
   {
-    title:'Aladin',
-    poster:'https://lh3.googleusercontent.com/proxy/CdFYuB3EnUhHndPsUCkCbBTk1L9GtKFB0hwS-tsbxjGeW_7uvv-oMwUMSG-NDI-uZQMALJdrBWUY2ATdYA-pTmMN5cUYCs40am7jcWjd07wai1-ixj9htQ'
+    title: "Aladin",
+    poster:
+      "https://lh3.googleusercontent.com/proxy/sdOoJ89RXZ5Q5LVFbfd009foWksqsY3IW9_1Ef2RJdEwDMrGP9ilVzbL7MVeMWYbuJfLPkPyaqY1W7BiRfRVqu035YpLIhmq0iQKnmwHpaNi8aBFLB1zMBhbk9bbeRpUAnH9AU-PyT2ton8BMkzZOEaQgmDjnkh7",
   },
   {
-    title:'Lion King',
-    poster:'https://cdn.vox-cdn.com/thumbor/PcGQL0F7Suz8p2cHF8lIjvC02Uc=/0x0:4096x2160/1200x800/filters:focal(1416x380:2070x1034)/cdn.vox-cdn.com/uploads/chorus_image/image/64749197/The_Lion_King_dt1_still_1__1_.0.jpg'
+    title: "Lion King",
+    poster:
+      "https://cdn.vox-cdn.com/thumbor/PcGQL0F7Suz8p2cHF8lIjvC02Uc=/0x0:4096x2160/1200x800/filters:focal(1416x380:2070x1034)/cdn.vox-cdn.com/uploads/chorus_image/image/64749197/The_Lion_King_dt1_still_1__1_.0.jpg",
   },
   {
-    title:'Mulan',
-    poster:'https://cdn.i-scmp.com/sites/default/files/styles/768x768/public/d8/images/methode/2019/07/31/b47721fc-adef-11e9-a61f-bc570b50c4e7_image_hires_154319.jpg?itok=VsX5RxDq&v=1564559004'
+    title: "Mulan",
+    poster:
+      "https://cdn.i-scmp.com/sites/default/files/styles/768x768/public/d8/images/methode/2019/07/31/b47721fc-adef-11e9-a61f-bc570b50c4e7_image_hires_154319.jpg?itok=VsX5RxDq&v=1564559004",
   },
   {
-    title:'Sleeping Beauty',
-    poster:'https://thegeekiary.com/wp-content/uploads/2019/08/sleeping-beauty-blu-ray.png'
-  }
-]
+    title: "Sleeping Beauty",
+    poster:
+      "https://thegeekiary.com/wp-content/uploads/2019/08/sleeping-beauty-blu-ray.png",
+  },
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <img id='img' src='https://1000logos.net/wp-content/uploads/2017/05/Walt-Disney-logo.png'/>
-       {movies.map(movie =>{    /*movies array 를 매핑해서 movies에있는 엘리먼트 만큼 사이클이 돌면서 컴포넌트를 만든다.
+        <img
+          id="img"
+          src="https://1000logos.net/wp-content/uploads/2017/05/Walt-Disney-logo.png"
+        />
+        {movies.map((movie, index) => {
+          /* index는 여기서 제공하는 리스트의 숫자를 의미.*/
+          /*movies array 를 매핑해서 movies에있는 엘리먼트 만큼 사이클이 돌면서 컴포넌트를 만든다.
         movies는 저 array 이고 movie는 각 배열의 0번방,1번방... 이다.*/
-         // eslint-disable-next-line no-unused-expressions
-        return <Movie title={movie.title} poster={movie.poster} />
-       })}
+          // eslint-disable-next-line no-unused-expressions
+          console.log("key>>", index);
+          return (
+            <Movie
+              title={movie.title}
+              poster={movie.poster}
+              key={index}
+            /> /* key로 id처럼 유니크한 속성을 부여함 보이지는 않음 !*/
+          );
+        })}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,26 +1,46 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types"; /* 원래 React안에 있었는데 분리됨. 그래서 따로 import해야한다.*/
 import "./Movie.css";
 
 class Movie extends Component {
-  render() {  /* component need to show something*/
+  static propTypes = {
+    /* 타입 설정*/
 
-    console.log('props>>',this.props)
+    title:
+      PropTypes.string
+        .isRequired /* isRequired를 하면 필수요건 이기때문에 null일경우 에러가 보여진다.*/,
+    poster: PropTypes.string.isRequired,
+  };
+
+  render() {
+    /* component need to show something*/
+
+    console.log("props>>", this.props);
     return (
-        <div>
-            <MoviePoster poster = {this.props.poster} />
-            <h1>{this.props.title}</h1 /* JSX의 명령을 실행시키려면 {} 괄호가 있어야함!*/>
-        </div>
-        );
+      <div>
+        <MoviePoster poster={this.props.poster} />
+        <h1>
+          {this.props.title}
+        </h1 /* JSX의 명령을 실행시키려면 {} 괄호가 있어야함!*/>
+      </div>
+    );
   }
 }
 
-class MoviePoster extends Component{
-    render(){
-        console.log('movie poster>>',this.props)
-        return(
-            <img src={this.props.poster} /* 부모에서 받은 props를 이용해서 출력함*//>
-        )
-    }
+class MoviePoster extends Component {
+  static propTypes = {
+    poster: PropTypes.string.isRequired,
+  };
+
+  render() {
+    console.log("movie poster>>", this.props);
+    return (
+      <img
+        src={this.props.poster}
+        alt="Movie Poster" /* 부모에서 받은 props를 이용해서 출력함*/
+      />
+    );
+  }
 }
 
 export default Movie;
